229062d4e0bc0276d8c4378be35cc59523ae16e4
Rifernun <rifernun@gmail.com>
2026-05-28 08:07:34 -0300

    Initializing Project

---
+++ src/main/java/com/richard/pocket_ledger/PocketLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.richard.pocket_ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richard/pocket_ledger/PocketLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richard.pocket_ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
